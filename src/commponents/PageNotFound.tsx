e4bb6e82c77802c1baee25c4b6e87ce911c5ed5d
Rodionova <daria.rodionova.909@gmail.com>
2021-08-10 23:19:06 +0300

    final UI

--- src/commponents/PageNotFound.tsx
+++ src/commponents/PageNotFound.tsx
@@ -1,8 +1,9 @@
 import React, { FunctionComponent } from 'react';
+import '../css/style.css';
 
 const PageNotFound: FunctionComponent = () => {
   return (
-    <div>
+    <div className="container">
       <h1>404 </h1>
       <h2>Page Not Found</h2>
     </div>
